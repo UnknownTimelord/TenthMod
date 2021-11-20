ae9dd4fed1114ad8bed5b96e072e71f0e5afe64c
UnknownTimelord <46335506+UnknownTimelord@users.noreply.github.com>
2021-11-19 19:20:47 -0700

    Initial commit

---
+++ src/main/java/com/tenth/tenthmod/TenthMod.java
@@ -0,0 +1,27 @@
+package com.tenth.tenthmod;
+
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(TenthMod.MOD_ID)
+public class TenthMod {
+    public static final String MOD_ID = "tenthmod";
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public TenthMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::setup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
